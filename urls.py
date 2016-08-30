a96f7d1a79e742432d528010271ad5fae5b0f2ea
Vladimir Grechukhin <vgrechyukhin@ptsecurity.com>
2016-08-30 18:57:26 +0300

    Add redirect

--- urls.py
+++ urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('authentication.urls')),
     url(r'^treasurer/', include('treasurer.urls')),
+    url(r'^.*$', RedirectView.as_view(url='treasurer/', permanent=False), name='index')
 ]
 
 if settings.DEBUG:
