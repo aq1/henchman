795109c7b214b9795a6b6f5672d040894701e353
aq1 <inmosh@protonmail.ch>
2017-11-20 03:01:38 +0300

    Fixed submit post view

--- blog/views/submit_post.py
+++ blog/views/submit_post.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 
 from blog.views import BaseView
@@ -15,12 +15,11 @@ class SubmitPost(BaseView):
             return redirect(reverse('blog_index'))
 
         if pk:
-            updated = Post.objects.filter(pk=pk, user=request.user).update(
-                title=title,
-                body=body,
-            )
-            if updated:
-                return redirect(reverse('view_post', kwargs={'pk': pk}))
+            post = get_object_or_404(Post, pk=pk, user=request.user)
+            post.title = title
+            post.body = body
+            post.save()
+            return redirect(reverse('view_post', kwargs={'pk': pk}))
         else:
             post = Post.objects.create(
                 user=request.user,
@@ -28,4 +27,3 @@ class SubmitPost(BaseView):
                 body=body,
             )
             return redirect(reverse('view_post', kwargs={'pk': post.pk}))
-        return redirect(reverse('blog_index'))
