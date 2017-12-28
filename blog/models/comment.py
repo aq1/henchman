549d31042c20e43f371e96ff38f61b666796e16e
aq1 <inmosh@protonmail.ch>
2017-12-28 04:44:20 +0300

    Alert vk chat when new comment is created

--- blog/models/comment.py
+++ blog/models/comment.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+import requests
+
 
 from blog.models import Post
 
@@ -22,3 +27,25 @@ class Comment(models.Model):
             user=self.user,
             post=self.post,
         )
+
+
+@receiver(post_save, sender=Comment)
+def send_notification_in_vk_chat(instance, created, **kwargs):
+    if not created:
+        return
+    try:
+        requests.post(
+            'https://api.vk.com/method/messages.send',
+            data={
+                'chat_id': settings.VK_CHAT_ID,
+                'message': '{} добавил новый комментарий к посту "{}"!'.format(
+                    instance.user,
+                    instance.post.title,
+                ),
+            },
+            params={
+                'access_token': settings.VK_TOKEN,
+            }
+        )
+    except requests.RequestException:
+        pass
